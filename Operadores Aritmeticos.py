5afea4f24848851f8eb2cb6ffcdcaf2b6a1a1f2d
Catalinafloresrojo <103601088+Catalinafloresrojo@users.noreply.github.com>
2022-04-13 17:07:08 -0400

    operadores aritmeticos y variables

---
+++ Operadores Aritmeticos.py
@@ -0,0 +1,26 @@
+print("suma")
+print(2 + 3)
+
+print("resta")
+print(5 - 3)
+
+print("multiplicacion")
+print(5 * 3)
+
+print("division")
+print(21 / 5)
+
+print("Division entera")
+print(21 // 5)
+
+print("Division modulo")
+print(21 % 5)
+
+print("exponente")
+print( 2** 3)
+
+# Reglas de procedencia
+print( 5 + 5 *2)
+
+
+
