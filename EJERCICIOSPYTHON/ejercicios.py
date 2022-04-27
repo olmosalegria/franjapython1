7ae3c4aa89c377a79c57bbc6c9aa535c8710ad51
Catalinafloresrojo <103601088+Catalinafloresrojo@users.noreply.github.com>
2022-04-27 16:55:24 -0400

    ejercicios python

---
+++ EJERCICIOSPYTHON/ejercicios.py
@@ -0,0 +1,34 @@
+#ejercicio 1
+# escribir un programa que pregunte el nombre por consola 
+# y un numero entero e imprimir por pantalla el nombre 
+# tantas veces como el numero que introdujo 
+
+#solucion 
+#nombre = input("ingrese su nombre")
+#numero = int("ingrese un numero: ")
+#print(nombre*numero)
+
+
+
+#ejercicio 2 
+#escribir un programa que muestre por pantalla el resultado 
+#de la siguiente operacion matematica
+
+#print(((3+2)/(2*5))**2)
+
+#ejercicio 3
+#escribir un programa que lea un entero positivo n 
+#introducido por el usuario y despues muestre por pantalla 
+# la suma de todos los enteros desde 1 hasta n 
+#la suma de los n primeros positivos puede ser calculada 
+#de la siguiente forma
+
+print("ingrese el valor de n:" )
+n = int(input("ingrese un valor positivo: "))
+print((n*(n+1))/2)
+suma = str(suma)
+n = str(n)
+print("la suma de los primeros numeros enteros desde 1 hasta " + n + "es" + suma)
+
+
+
